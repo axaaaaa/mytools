cf5792071c771753c8be10c0c787b75f16790dba
01 <66402682+axaaaaa@users.noreply.github.com>
2024-01-28 06:22:58 +0800

    Add files via upload

---
+++ 浏览器打开，文件夹打开.py
@@ -0,0 +1,6 @@
+import subprocess
+subprocess.Popen(r"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
+subprocess.Popen(r"C:\Program Files (x86)\WeGame\wegame.exe")
+folder_path = "E:\\notes\\"
+subprocess.Popen(["explorer.exe", folder_path])     //打开指定文件夹
+
